0faec384fb6fb46d322fb55a07e0d47c6f0d3dfd
Maoyuan <295608936@qq.com>
2019-10-24 15:46:32 +0800

    ok

--- src/main/java/com/github/hcsp/stream/Problem2.java
+++ src/main/java/com/github/hcsp/stream/Problem2.java
@@ -3,12 +3,13 @@ package com.github.hcsp.stream;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class Problem2 {
     // 判断一段文本中是否包含关键词列表中的文本，如果包含任意一个关键词，返回true，否则返回false
     // 例如，text="catcatcat,boyboyboy", keywords=["boy", "girl"]，返回true
     // 例如，text="I am a boy", keywords=["cat", "dog"]，返回false
     public static boolean containsKeyword(String text, List<String> keywords) {
-        return false;
+        return keywords.stream().anyMatch(text::contains);
     }
 
     public static void main(String[] args) {
