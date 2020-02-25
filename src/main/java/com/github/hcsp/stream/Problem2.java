5d0e92f47ee6aee21a84ff2ea8d352483b522fb6
hukai <huzhengen@163.com>
2020-02-25 16:30:10 +0800

    Stream练习

--- src/main/java/com/github/hcsp/stream/Problem2.java
+++ src/main/java/com/github/hcsp/stream/Problem2.java
@@ -8,7 +8,7 @@ public class Problem2 {
     // 例如，text="catcatcat,boyboyboy", keywords=["boy", "girl"]，返回true
     // 例如，text="I am a boy", keywords=["cat", "dog"]，返回false
     public static boolean containsKeyword(String text, List<String> keywords) {
-        return false;
+        return keywords.stream().anyMatch(text::contains);
     }
 
     public static void main(String[] args) {
