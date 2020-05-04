a3e3b83141d3da5b34681ca328e6f89fb17bdc31
wenzhe <mysteryven@gmail.com>
2020-05-04 09:15:17 +0800

    使用流简化编程，确实简单了好多！

--- src/main/java/com/github/hcsp/stream/Problem1.java
+++ src/main/java/com/github/hcsp/stream/Problem1.java
@@ -1,8 +1,7 @@
 package com.github.hcsp.stream;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem1 {
     static class User {
@@ -13,16 +12,22 @@ public class Problem1 {
             this.name = name;
             this.age = age;
         }
+
+      public int getAge() {
+        return age;
+      }
     }
 
+
     // 编写一个方法，统计"年龄大于等于60的用户中，名字是两个字的用户数量"
-    public static int countUsers(List<User> users) {
-        return 0;
+   public static int countUsers(List<User> users) {
+      return users.stream().filter(user -> user.age >= 60 && user.name.length() ==2).toArray().length;
     }
 
     // 编写一个方法，筛选出年龄大于等于60的用户，然后将他们按照年龄从大到小排序，将他们的名字放在一个LinkedList中返回
     public static LinkedList<String> collectNames(List<User> users) {
-        return null;
+      return users.stream().filter(user -> user.age >= 60)
+          .sorted(Comparator.comparing(User::getAge).reversed()).map(user -> user.name).collect(Collectors.toCollection(LinkedList::new));
     }
 
     public static void main(String[] args) {
