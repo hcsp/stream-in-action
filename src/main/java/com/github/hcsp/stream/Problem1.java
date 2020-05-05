61694666dec4879dfc26a0572d4c29ba1d9811aa
Sunpeng526 <215860865@qq.com>
2020-05-04 20:36:12 -0700

    stream execise

--- src/main/java/com/github/hcsp/stream/Problem1.java
+++ src/main/java/com/github/hcsp/stream/Problem1.java
@@ -1,13 +1,45 @@
 package com.github.hcsp.stream;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem1 {
     static class User {
+
         private String name;
+
         private int age;
+
+        /**
+         * @return the name
+         */
+        public String getName() {
+            return name;
+        }
+
+        /**
+         * @param name the name to set
+         */
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        /**
+         * @return the age
+         */
+        public int getAge() {
+            return age;
+        }
+
+        /**
+         * @param age the age to set
+         */
+        public void setAge(int age) {
+            this.age = age;
+        }
 
         User(String name, int age) {
             this.name = name;
@@ -15,31 +47,27 @@ public class Problem1 {
         }
     }
 
-    // 编写一个方法，统计"年龄大于等于60的用户中，名字是两个字的用户数量"
+    /*
+     * 编写一个方法，统计"年龄大于等于60的用户中，名字是两个字的用户数量"
+     */
     public static int countUsers(List<User> users) {
-        return 0;
+        return users.stream().filter(p -> p.age >= 60 && p.name.split("").length == 2).collect(Collectors.toList())
+                .size();
     }
 
-    // 编写一个方法，筛选出年龄大于等于60的用户，然后将他们按照年龄从大到小排序，将他们的名字放在一个LinkedList中返回
+    /*
+     * 编写一个方法，筛选出年龄大于等于60的用户，然后将他们按照年龄从大到小排序，将他们的名字放在一个LinkedList中返回
+     */
     public static LinkedList<String> collectNames(List<User> users) {
-        return null;
+        return users.stream().filter(p -> p.age >= 60).sorted(Comparator.comparing(User::getAge).reversed()).map(User::getName)
+                .collect(Collectors.toCollection(LinkedList::new));
     }
 
     public static void main(String[] args) {
-        System.out.println(
-                countUsers(
-                        Arrays.asList(
-                                new User("张三", 60),
-                                new User("李四", 61),
-                                new User("张三丰", 300),
-                                new User("王五", 12))));
+        System.out.println(countUsers(
+                Arrays.asList(new User("张三", 60), new User("李四", 61), new User("张三丰", 300), new User("王五", 12))));
 
-        System.out.println(
-                collectNames(
-                        Arrays.asList(
-                                new User("张三", 60),
-                                new User("李四", 61),
-                                new User("张三丰", 300),
-                                new User("王五", 12))));
+        System.out.println(collectNames(
+                Arrays.asList(new User("张三", 60), new User("李四", 61), new User("张三丰", 300), new User("王五", 12))));
     }
 }
