a4527ddc612fb094ecf45b537cfdd17fd1bd6cc4
async <lingyidx@gmail.com>
2020-05-20 21:27:20 +0800

    完成 (#105)
    完成 (#105)

--- src/main/java/com/github/hcsp/stream/Problem1.java
+++ src/main/java/com/github/hcsp/stream/Problem1.java
@@ -1,8 +1,7 @@
 package com.github.hcsp.stream;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem1 {
     static class User {
@@ -13,16 +12,39 @@ public class Problem1 {
             this.name = name;
             this.age = age;
         }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
     }
 
     // 编写一个方法，统计"年龄大于等于60的用户中，名字是两个字的用户数量"
     public static int countUsers(List<User> users) {
-        return 0;
+        return (int) users.stream()
+                .filter(var -> var.age >= 60)
+                .filter(var -> var.name.length() == 2)
+                .count();
     }
 
     // 编写一个方法，筛选出年龄大于等于60的用户，然后将他们按照年龄从大到小排序，将他们的名字放在一个LinkedList中返回
     public static LinkedList<String> collectNames(List<User> users) {
-        return null;
+        List<String> list = users.stream().filter(user -> user.age >= 60)
+                .sorted(Comparator.comparingInt(User::getAge).reversed())
+                .map(User::getName)
+                .collect(Collectors.toList());
+        return new LinkedList(list);
     }
 
     public static void main(String[] args) {
