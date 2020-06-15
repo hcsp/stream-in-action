4a289d7fda6d4eaf41ecf276dbaaf34b384ef462
lance <491799643@qq.com>
2020-06-15 18:16:00 +0800

    Stream流的练习

--- src/main/java/com/github/hcsp/stream/Problem6.java
+++ src/main/java/com/github/hcsp/stream/Problem6.java
@@ -3,17 +3,18 @@ package com.github.hcsp.stream;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Problem6 {
-    // 使用流的方法，把所有长度等于1的单词挑出来，然后用逗号连接起来
-    // 例如，传入参数words=['a','bb','ccc','d','e']
-    // 返回字符串a,d,e
-    public static String filterThenConcat(Set<String> words) {
-        return null;
-    }
+  // 使用流的方法，把所有长度等于1的单词挑出来，然后用逗号连接起来
+  // 例如，传入参数words=['a','bb','ccc','d','e']
+  // 返回字符串a,d,e
+  public static String filterThenConcat(Set<String> words) {
+    return words.stream().filter(ch -> ch.length() == 1).collect(Collectors.joining(","));
+  }
 
-    public static void main(String[] args) {
-        Set<String> set = new LinkedHashSet<>(Arrays.asList("a", "bb", "ccc", "d", "e"));
-        System.out.println(filterThenConcat(set));
-    }
+  public static void main(String[] args) {
+    Set<String> set = new LinkedHashSet<>(Arrays.asList("a", "bb", "ccc", "d", "e"));
+    System.out.println(filterThenConcat(set));
+  }
 }
