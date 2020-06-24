d6a62ca07538189050fd07e818db610f51653298
Haixiang <haixiang6123@gmail.com>
2020-06-24 15:03:03 +0800

    Stream

--- src/main/java/com/github/hcsp/stream/Problem3.java
+++ src/main/java/com/github/hcsp/stream/Problem3.java
@@ -5,7 +5,9 @@ public class Problem3 {
     // 统计一个给定的字符串中，大写英文字母（A,B,C,...,Z）出现的次数。
     // 例如，给定字符串"AaBbCc1234ABC"，返回6，因为该字符串中出现了6次大写英文字母ABCABC
     public static int countUpperCaseLetters(String str) {
-        return 0;
+        return (int) str.chars()
+                .filter(ch -> Character.isUpperCase(ch))
+                .count();
     }
 
     public static void main(String[] args) {
