4cdc5a94ca491938f24100023e20686944b1ec15
concise <15521608275@163.com>
2020-07-16 11:38:44 +0800

    java8 的 stream
    里面的方法还不是很熟悉

--- src/main/java/com/github/hcsp/stream/Problem2.java
+++ src/main/java/com/github/hcsp/stream/Problem2.java
@@ -8,7 +8,9 @@ public class Problem2 {
     // 例如，text="catcatcat,boyboyboy", keywords=["boy", "girl"]，返回true
     // 例如，text="I am a boy", keywords=["cat", "dog"]，返回false
     public static boolean containsKeyword(String text, List<String> keywords) {
-        return false;
+//        return false;  我想把text作为stream然后和keywords对比，但是好像text没法作为stream，这是为什么
+//keywords.stream().
+        return keywords.stream().anyMatch(text::contains);
     }
 
     public static void main(String[] args) {
