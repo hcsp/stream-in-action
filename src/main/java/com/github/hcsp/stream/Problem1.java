6f44337ba9654fca17c128cad48ed2c7d4b3e0ef
Ryan <2433005622@qq.com>
2020-08-12 18:14:49 +0800

    函数式编程：Stream练习

--- src/main/java/com/github/hcsp/stream/Problem1.java
+++ src/main/java/com/github/hcsp/stream/Problem1.java
@@ -1,6 +1,10 @@
 package com.github.hcsp.stream;
 
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Problem1 {
--- src/main/java/com/github/hcsp/stream/Problem4.java
+++ src/main/java/com/github/hcsp/stream/Problem4.java
@@ -1,7 +1,12 @@
 package com.github.hcsp.stream;
 
 
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Problem4 {
