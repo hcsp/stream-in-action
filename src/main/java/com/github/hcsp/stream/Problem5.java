a7307ddef4ade07c7e417a93329ecf0094f06ac0
DING-Weibing <57584830+DING-Weibing@users.noreply.github.com>
2020-08-26 04:25:07 +0900

    my commit (#128)
    my commit (#128)

--- src/main/java/com/github/hcsp/stream/Problem5.java
+++ src/main/java/com/github/hcsp/stream/Problem5.java
@@ -1,15 +1,14 @@
 package com.github.hcsp.stream;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem5 {
     // 使用流的方法，把订单处理成ID->订单的映射
     // 例如，传入参数[{id=1,name='肥皂'},{id=2,name='牙刷'}]
     // 返回一个映射{1->Order(1,'肥皂'),2->Order(2,'牙刷')}
     public static Map<Integer, Order> toMap(List<Order> orders) {
-        return null;
+        return orders.parallelStream().collect(Collectors.toMap(Order::getId, order -> order));
     }
 
     public static void main(String[] args) {
@@ -31,6 +30,14 @@ public class Problem5 {
 
         public String getName() {
             return name;
+        }
+
+        @Override
+        public String toString() {
+            return "Order{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
         }
     }
 }
