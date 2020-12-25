8876a141997aeb527a254964a1c34c4a33c3ecfe
daimon0809 <51164058+daimon0809@users.noreply.github.com>
2020-12-25 20:57:12 +0800

    函数式编程练习：Stream (#143)
    函数式编程练习：Stream (#143)

--- src/main/java/com/github/hcsp/stream/Problem5.java
+++ src/main/java/com/github/hcsp/stream/Problem5.java
@@ -3,13 +3,15 @@ package com.github.hcsp.stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Problem5 {
     // 使用流的方法，把订单处理成ID->订单的映射
     // 例如，传入参数[{id=1,name='肥皂'},{id=2,name='牙刷'}]
     // 返回一个映射{1->Order(1,'肥皂'),2->Order(2,'牙刷')}
     public static Map<Integer, Order> toMap(List<Order> orders) {
-        return null;
+        return orders.stream()
+                .collect(Collectors.toMap(Order::getId, order -> order));
     }
 
     public static void main(String[] args) {
