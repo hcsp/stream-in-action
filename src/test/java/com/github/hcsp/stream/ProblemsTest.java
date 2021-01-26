afa5754691e93e0c87e87bbdcd1757961a2d61c2
breeze <1023349760@qq.com>
2021-01-27 00:13:05 +0800

    完成修改

--- src/test/java/com/github/hcsp/stream/ProblemsTest.java
+++ src/test/java/com/github/hcsp/stream/ProblemsTest.java
@@ -1,10 +1,13 @@
 package com.github.hcsp.stream;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.util.*;
-import java.util.stream.Collectors;
 
 public class ProblemsTest {
     @Test
