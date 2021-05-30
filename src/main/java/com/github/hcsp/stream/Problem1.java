2e47bd85be39f5895477521691a4f07cd089f9c8
yiweiran <168578203@qq.com>
2021-05-30 23:18:20 +0800

    流的运用

--- src/main/java/com/github/hcsp/stream/Problem1.java
+++ src/main/java/com/github/hcsp/stream/Problem1.java
@@ -4,10 +4,29 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.*;
+
 public class Problem1 {
     static class User {
         private String name;
         private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
 
         User(String name, int age) {
             this.name = name;
@@ -17,12 +36,25 @@ public class Problem1 {
 
     // 编写一个方法，统计"年龄大于等于60的用户中，名字是两个字的用户数量"
     public static int countUsers(List<User> users) {
-        return 0;
+        return (int) users.stream()
+                .filter(user -> user.age >= 60 && user.name.length() == 2)
+                .count();
+
     }
 
     // 编写一个方法，筛选出年龄大于等于60的用户，然后将他们按照年龄从大到小排序，将他们的名字放在一个LinkedList中返回
     public static LinkedList<String> collectNames(List<User> users) {
-        return null;
+        return users.stream()
+                // 过滤
+                .filter(user -> user.age >= 60)
+                // 排序
+                .sorted(comparing(User::getAge).reversed())
+                // 映射
+                .map(User::getName)
+                // 收集
+                .collect(toCollection(LinkedList::new)
+                );
+
     }
 
     public static void main(String[] args) {
