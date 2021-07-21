6a4aabfddf6f67f23746aaafcfb5c694122592f6
bowen-wu <13793248162@163.com>
2021-07-21 16:05:32 +0800

    feat: complete Stream

--- src/main/java/com/github/hcsp/stream/Problem6.java
+++ src/main/java/com/github/hcsp/stream/Problem6.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import static java.util.stream.Collectors.joining;
+
 public class Problem6 {
     // 使用流的方法，把所有长度等于1的单词挑出来，然后用逗号连接起来
     // 例如，传入参数words=['a','bb','ccc','d','e']
     // 返回字符串a,d,e
     public static String filterThenConcat(Set<String> words) {
-        return null;
+        return words.stream().filter(str -> str.length() == 1).collect(joining(","));
     }
 
     public static void main(String[] args) {
