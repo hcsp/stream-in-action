bed3f4b451b6de20863f43623857f7beebc2d530
MongieLee <le30n@outlook.com>
2021-11-04 17:07:06 +0800

    函数式编程练习：Stream

--- src/main/java/com/github/hcsp/stream/Problem4.java
+++ src/main/java/com/github/hcsp/stream/Problem4.java
@@ -1,9 +1,7 @@
 package com.github.hcsp.stream;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Problem4 {
     // 再用流的方法把之前的题目做一遍吧：
@@ -15,7 +13,9 @@ public class Problem4 {
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
     public static Map<String, List<Employee>> collect(List<Employee> employees) {
-        return null;
+        return employees.stream()
+                .sorted(Comparator.comparing(Employee::getAge))
+                .collect(Collectors.groupingBy(Employee::getDepartment));
     }
 
     public static void main(String[] args) {
@@ -58,6 +58,16 @@ public class Problem4 {
 
         public String getDepartment() {
             return department;
+        }
+
+        @Override
+        public String toString() {
+            return "Employee{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", age=" + age +
+                    ", department='" + department + '\'' +
+                    '}';
         }
 
         @Override
