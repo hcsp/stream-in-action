a9580738f553a87467a8e8884a10b0901e848ed2
zdmhahaha <987854018@qq.com>
2021-12-17 23:35:55 +0800

    函数式编程练习：Stream

--- src/main/java/com/github/hcsp/stream/Problem6.java
+++ src/main/java/com/github/hcsp/stream/Problem6.java
@@ -3,13 +3,14 @@ package com.github.hcsp.stream;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     // 使用流的方法，把所有长度等于1的单词挑出来，然后用逗号连接起来
     // 例如，传入参数words=['a','bb','ccc','d','e']
     // 返回字符串a,d,e
     public static String filterThenConcat(Set<String> words) {
-        return null;
+        return words.stream().filter(s -> s.length() == 1).collect(Collectors.joining(","));
     }
 
     public static void main(String[] args) {
