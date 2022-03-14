ff372f8d1786681706e6818191b1e8a5015f275f
freetie <freetie@qq.com>
2022-03-14 22:52:05 +0800

    finish Problems

--- src/main/java/com/github/hcsp/stream/Problem5.java
+++ src/main/java/com/github/hcsp/stream/Problem5.java
@@ -1,15 +1,24 @@
 package com.github.hcsp.stream;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 
 public class Problem5 {
     // 使用流的方法，把订单处理成ID->订单的映射
     // 例如，传入参数[{id=1,name='肥皂'},{id=2,name='牙刷'}]
     // 返回一个映射{1->Order(1,'肥皂'),2->Order(2,'牙刷')}
     public static Map<Integer, Order> toMap(List<Order> orders) {
-        return null;
+        return orders.stream().reduce(new HashMap<>(), (integerOrderHashMap, order) -> {
+            integerOrderHashMap.put(order.getId(), order);
+            return integerOrderHashMap;
+        }, (integerOrderHashMap, integerOrderHashMap2) -> {
+            integerOrderHashMap.putAll(integerOrderHashMap2);
+            return integerOrderHashMap;
+        });
     }
 
     public static void main(String[] args) {
