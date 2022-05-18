2b54350c09ff31274ae6abc9a288139a57fe56a4
xiangqiangkan <2180531663@qq.com>
2022-05-18 17:19:11 +0800

    stream方法采购

--- src/main/java/com/github/hcsp/stream/Problem6.java
+++ src/main/java/com/github/hcsp/stream/Problem6.java
@@ -1,19 +1,21 @@
 package com.github.hcsp.stream;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     // 使用流的方法，把所有长度等于1的单词挑出来，然后用逗号连接起来
     // 例如，传入参数words=['a','bb','ccc','d','e']
     // 返回字符串a,d,e
     public static String filterThenConcat(Set<String> words) {
-        return null;
+        return words.stream().filter(i -> i.length() == 1).collect(Collectors.joining(","));
     }
 
     public static void main(String[] args) {
-        Set<String> set = new LinkedHashSet<>(Arrays.asList("a", "bb", "ccc", "d", "e"));
-        System.out.println(filterThenConcat(set));
+
+        System.out.println(filterThenConcat(new HashSet<>(Arrays.asList("a", "bb", "ccc", "d", "e"))));
     }
 }
