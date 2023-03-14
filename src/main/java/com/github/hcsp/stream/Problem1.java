eba3caac17be2a2a07749f8baafc7b8ad32baf15
fanpeichao <fan552426811@outlook.com>
2023-03-14 10:26:28 +0800

    完成任务

--- src/main/java/com/github/hcsp/stream/Problem1.java
+++ src/main/java/com/github/hcsp/stream/Problem1.java
@@ -19,8 +19,8 @@ public class Problem1 {
 
     // 编写一个方法，统计"年龄大于等于60的用户中，名字是两个字的用户数量"
     public static int countUsers(List<User> users) {
+//        Arrays.sort(a);
         return (int) users.stream().filter(user -> user.age >= 60).filter(user -> user.name.length() == 2).count();
-
     }
 
     // 编写一个方法，筛选出年龄大于等于60的用户，然后将他们按照年龄从大到小排序，将他们的名字放在一个LinkedList中返回
